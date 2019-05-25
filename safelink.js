e5cadbd36f80a2de15e44afc0be514df0c5a197b
mizyan123 <41173393+mizyan123@users.noreply.github.com>
2019-05-25 16:56:47 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -32,7 +32,8 @@ if(x=="8"){xxx="natural-mineral-makeup-3-reasons-why-it.html"}
 if(x=="9"){xxx="dry-skin-care.html"}
 if(x=="10"){xxx="organic-bath-soaps.html"}
 if(x=="11"){xxx="organic-shampoo.html"}
-if(x=="12"){xxx="trampoline-basics.html"}a_to_ve[i].href="https://www.page.robotbambu.com/2018/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="12"){xxx="trampoline-basics.html"}
+a_to_ve[i].href="https://www.page.robotbambu.com/2018/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
